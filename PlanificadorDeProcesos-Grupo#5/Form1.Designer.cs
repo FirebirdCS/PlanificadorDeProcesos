27cd505b0f662e47fbbeaf6e0713452cd88707d4
AbelSánchez333 <asancheza9@mium.edu.gt>
2024-09-16 18:02:00 -0600

    Actualizar procesos REALIZADO

--- PlanificadorDeProcesos-Grupo#5/Form1.Designer.cs
+++ PlanificadorDeProcesos-Grupo#5/Form1.Designer.cs
@@ -39,7 +39,7 @@
             this.btnSalir = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.txtContador = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
@@ -58,6 +58,7 @@
             this.dataGridView2.RowTemplate.Height = 24;
             this.dataGridView2.Size = new System.Drawing.Size(1367, 567);
             this.dataGridView2.TabIndex = 1;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
             // 
             // dgvProceso
             // 
@@ -102,6 +103,7 @@
             this.btnActualizar.TabIndex = 2;
             this.btnActualizar.Text = "Actualizar";
             this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
             // 
             // btnDetener
             // 
@@ -126,7 +128,7 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(1220, 733);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(18, 20);
+            this.label1.Size = new System.Drawing.Size(14, 16);
             this.label1.TabIndex = 5;
             this.label1.Text = "0";
             // 
@@ -135,26 +137,27 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(1050, 729);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(164, 20);
+            this.label2.Size = new System.Drawing.Size(131, 16);
             this.label2.TabIndex = 6;
             this.label2.Text = "Nombre del proceso";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
-            // label3
+            // txtContador
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(76, 748);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(44, 16);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "label3";
+            this.txtContador.AutoSize = true;
+            this.txtContador.Location = new System.Drawing.Point(76, 748);
+            this.txtContador.Name = "txtContador";
+            this.txtContador.Size = new System.Drawing.Size(44, 16);
+            this.txtContador.TabIndex = 7;
+            this.txtContador.Text = "label3";
+            this.txtContador.Click += new System.EventHandler(this.label3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1391, 784);
-            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtContador);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnSalir);
@@ -183,7 +186,7 @@
         private System.Windows.Forms.Button btnSalir;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label txtContador;
     }
 }
 
--- PlanificadorDeProcesos-Grupo#5/Form1.cs
+++ PlanificadorDeProcesos-Grupo#5/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,22 @@ namespace PlanificadorDeProcesos_Grupo_5
         public Form1()
         {
             InitializeComponent();
+            UpdateProcessList();
+        }
+
+        private void UpdateProcessList(){
+
+            dataGridView2.Rows.Clear();
+            foreach (Process p in Process.GetProcesses())
+            {
+                int n = dataGridView2.Rows.Add();
+                dataGridView2.Rows[n].Cells[0].Value = p.ProcessName;
+                dataGridView2.Rows[n].Cells[1].Value = p.Id;
+                dataGridView2.Rows[n].Cells[2].Value = p.WorkingSet64;
+                dataGridView2.Rows[n].Cells[3].Value = p.VirtualMemorySize64;
+                dataGridView2.Rows[n].Cells[4].Value = p.SessionId;
+            }
+            txtContador.Text = "Procesos actuales: " + dataGridView2.Rows.Count.ToString();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -23,6 +40,21 @@ namespace PlanificadorDeProcesos_Grupo_5
         }
 
         private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            UpdateProcessList();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
         {
 
         }
