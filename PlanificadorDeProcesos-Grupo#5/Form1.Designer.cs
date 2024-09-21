f647ddb31a0c5450f5668921819f39c9306ce692
Juanka20-Sandoval00 <jsandovalq@miumg.edu.gt>
2024-09-20 21:45:19 -0600

    Proyecto terminado

--- PlanificadorDeProcesos-Grupo#5/Form1.Designer.cs
+++ PlanificadorDeProcesos-Grupo#5/Form1.Designer.cs
@@ -28,23 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
-            this.dgvProceso = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dgvID = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dgvFisica = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dgvVirtual = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dgvCPU = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.btnActualizar = new System.Windows.Forms.Button();
             this.btnDetener = new System.Windows.Forms.Button();
             this.btnSalir = new System.Windows.Forms.Button();
             this.txtProceso = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.txtContador = new System.Windows.Forms.Label();
+            this.dgvProceso = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgvID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgvFisica = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgvVirtual = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgvCPU = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView2
             // 
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Arial", 10F);
+            this.dataGridView2.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView2.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.dgvProceso,
@@ -52,7 +69,8 @@
             this.dgvFisica,
             this.dgvVirtual,
             this.dgvCPU});
-            this.dataGridView2.Location = new System.Drawing.Point(8, 51);
+            this.dataGridView2.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.dataGridView2.Location = new System.Drawing.Point(1, 53);
             this.dataGridView2.Margin = new System.Windows.Forms.Padding(2);
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.RowHeadersWidth = 51;
@@ -61,72 +79,43 @@
             this.dataGridView2.TabIndex = 1;
             this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
             // 
-            // dgvProceso
-            // 
-            this.dgvProceso.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.dgvProceso.HeaderText = "Proceso";
-            this.dgvProceso.MinimumWidth = 6;
-            this.dgvProceso.Name = "dgvProceso";
-            // 
-            // dgvID
-            // 
-            this.dgvID.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.dgvID.HeaderText = "ID";
-            this.dgvID.MinimumWidth = 6;
-            this.dgvID.Name = "dgvID";
-            // 
-            // dgvFisica
-            // 
-            this.dgvFisica.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.dgvFisica.HeaderText = "Memoria Física";
-            this.dgvFisica.MinimumWidth = 6;
-            this.dgvFisica.Name = "dgvFisica";
-            // 
-            // dgvVirtual
-            // 
-            this.dgvVirtual.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.dgvVirtual.HeaderText = "Memoria Virtual";
-            this.dgvVirtual.MinimumWidth = 6;
-            this.dgvVirtual.Name = "dgvVirtual";
-            // 
-            // dgvCPU
-            // 
-            this.dgvCPU.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.dgvCPU.HeaderText = "CPU";
-            this.dgvCPU.MinimumWidth = 6;
-            this.dgvCPU.Name = "dgvCPU";
-            // 
             // btnActualizar
             // 
-            this.btnActualizar.Location = new System.Drawing.Point(28, 535);
+            this.btnActualizar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnActualizar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnActualizar.Location = new System.Drawing.Point(57, 535);
             this.btnActualizar.Margin = new System.Windows.Forms.Padding(2);
             this.btnActualizar.Name = "btnActualizar";
-            this.btnActualizar.Size = new System.Drawing.Size(236, 54);
+            this.btnActualizar.Size = new System.Drawing.Size(241, 51);
             this.btnActualizar.TabIndex = 2;
             this.btnActualizar.Text = "Actualizar";
-            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.UseVisualStyleBackColor = false;
             this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
             // 
             // btnDetener
             // 
-            this.btnDetener.Location = new System.Drawing.Point(296, 535);
+            this.btnDetener.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnDetener.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDetener.Location = new System.Drawing.Point(435, 535);
             this.btnDetener.Margin = new System.Windows.Forms.Padding(2);
             this.btnDetener.Name = "btnDetener";
             this.btnDetener.Size = new System.Drawing.Size(236, 54);
             this.btnDetener.TabIndex = 3;
             this.btnDetener.Text = "Detener";
-            this.btnDetener.UseVisualStyleBackColor = true;
+            this.btnDetener.UseVisualStyleBackColor = false;
             this.btnDetener.Click += new System.EventHandler(this.btnDetener_Click);
             // 
             // btnSalir
             // 
-            this.btnSalir.Location = new System.Drawing.Point(772, 535);
+            this.btnSalir.BackColor = System.Drawing.Color.LightCoral;
+            this.btnSalir.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(790, 535);
             this.btnSalir.Margin = new System.Windows.Forms.Padding(2);
             this.btnSalir.Name = "btnSalir";
             this.btnSalir.Size = new System.Drawing.Size(236, 54);
             this.btnSalir.TabIndex = 4;
             this.btnSalir.Text = "Salir";
-            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.UseVisualStyleBackColor = false;
             this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
             // 
             // txtProceso
@@ -142,18 +131,18 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(788, 592);
+            this.label2.Location = new System.Drawing.Point(806, 596);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(102, 13);
+            this.label2.Size = new System.Drawing.Size(105, 13);
             this.label2.TabIndex = 6;
-            this.label2.Text = "Nombre del proceso";
+            this.label2.Text = "Nombre del proceso:";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // txtContador
             // 
             this.txtContador.AutoSize = true;
-            this.txtContador.Location = new System.Drawing.Point(57, 608);
+            this.txtContador.Location = new System.Drawing.Point(79, 596);
             this.txtContador.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.txtContador.Name = "txtContador";
             this.txtContador.Size = new System.Drawing.Size(35, 13);
@@ -161,11 +150,76 @@
             this.txtContador.Text = "label3";
             this.txtContador.Click += new System.EventHandler(this.label3_Click);
             // 
+            // dgvProceso
+            // 
+            this.dgvProceso.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Arial", 10F);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.LightGray;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.Black;
+            this.dgvProceso.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvProceso.HeaderText = "Proceso";
+            this.dgvProceso.MinimumWidth = 6;
+            this.dgvProceso.Name = "dgvProceso";
+            // 
+            // dgvID
+            // 
+            this.dgvID.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.LemonChiffon;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Arial", 10F);
+            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.Color.LightGray;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.Color.Black;
+            this.dgvID.DefaultCellStyle = dataGridViewCellStyle4;
+            this.dgvID.HeaderText = "ID";
+            this.dgvID.MinimumWidth = 6;
+            this.dgvID.Name = "dgvID";
+            // 
+            // dgvFisica
+            // 
+            this.dgvFisica.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle5.BackColor = System.Drawing.Color.LemonChiffon;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Arial", 10F);
+            dataGridViewCellStyle5.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.Color.LightGray;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.Color.Black;
+            this.dgvFisica.DefaultCellStyle = dataGridViewCellStyle5;
+            this.dgvFisica.HeaderText = "Memoria Física";
+            this.dgvFisica.MinimumWidth = 6;
+            this.dgvFisica.Name = "dgvFisica";
+            // 
+            // dgvVirtual
+            // 
+            this.dgvVirtual.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle6.BackColor = System.Drawing.Color.LemonChiffon;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Arial", 10F);
+            dataGridViewCellStyle6.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.Color.LightGray;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.Color.Black;
+            this.dgvVirtual.DefaultCellStyle = dataGridViewCellStyle6;
+            this.dgvVirtual.HeaderText = "Memoria Virtual";
+            this.dgvVirtual.MinimumWidth = 6;
+            this.dgvVirtual.Name = "dgvVirtual";
+            // 
+            // dgvCPU
+            // 
+            this.dgvCPU.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle7.BackColor = System.Drawing.Color.LemonChiffon;
+            dataGridViewCellStyle7.Font = new System.Drawing.Font("Arial", 10F);
+            dataGridViewCellStyle7.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle7.SelectionBackColor = System.Drawing.Color.LightGray;
+            dataGridViewCellStyle7.SelectionForeColor = System.Drawing.Color.Black;
+            this.dgvCPU.DefaultCellStyle = dataGridViewCellStyle7;
+            this.dgvCPU.HeaderText = "CPU";
+            this.dgvCPU.MinimumWidth = 6;
+            this.dgvCPU.Name = "dgvCPU";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1043, 637);
+            this.ClientSize = new System.Drawing.Size(1051, 637);
             this.Controls.Add(this.txtContador);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtProceso);
@@ -177,6 +231,7 @@
             this.Name = "Form1";
             this.Padding = new System.Windows.Forms.Padding(15, 60, 15, 16);
             this.Text = "Administrador de Tareas";
+            this.TransparencyKey = System.Drawing.Color.Empty;
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
@@ -187,17 +242,17 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView2;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dgvProceso;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dgvID;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dgvFisica;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dgvVirtual;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dgvCPU;
         private System.Windows.Forms.Button btnActualizar;
         private System.Windows.Forms.Button btnDetener;
         private System.Windows.Forms.Button btnSalir;
         private System.Windows.Forms.Label txtProceso;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label txtContador;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgvProceso;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgvID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgvFisica;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgvVirtual;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgvCPU;
     }
 }
 
